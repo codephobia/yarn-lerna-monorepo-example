abd33a19c3ec314b9556994f218403cbbc484408
Martin Raymond <codephobia@hotmail.com>
2020-04-08 23:23:14 -0700

    feat(logger): added info logging

--- packages/logger/src/index.js
+++ packages/logger/src/index.js
@@ -4,11 +4,16 @@ const log = (str) => {
     console.log(str);
 };
 
+const info = (str) => {
+    console.info(str);
+};
+
 const shuffle = (arr) => {
     return _.shuffle(arr);
 }
 
 module.exports = {
     log,
+    info,
     shuffle,
 };
