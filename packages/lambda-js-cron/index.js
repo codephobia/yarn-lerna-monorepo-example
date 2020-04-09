f94f5efdb41fb4921e74c740718fb5b5ee132b61
Martin Raymond <codephobia@hotmail.com>
2020-04-08 23:04:58 -0700

    lambda-js-cron: update response

--- packages/lambda-js-cron/index.js
+++ packages/lambda-js-cron/index.js
@@ -4,5 +4,5 @@ const logger = require('@codephobia/logger');
 
 exports.handler = async function (event, context) {
     logger.log(`envkey: ${process.env.ENVKEY}`);
-    return logger.shuffle([1, 2, 3, 4, 5]);
+    return logger.shuffle([1, 2, 3, 4, 5, 6]);
 }
